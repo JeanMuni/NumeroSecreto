9de1517e79534703a5420076da434bafa4145c4c
JeanMuni <144803173+JeanMuni@users.noreply.github.com>
2025-11-22 19:53:13 -0300

    .

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let numeroSecreto = 5;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
 
-// se chute for igual ao número secreto
 if (chute == numeroSecreto) {
     alert(`Isso ai! você descobriu o numero secreto ${numeroSecreto} `);
 } else {
