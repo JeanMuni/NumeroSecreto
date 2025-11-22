27962e978f47e9ad867d98936efef7f16a05372a
JeanMuni <144803173+JeanMuni@users.noreply.github.com>
2025-11-22 19:45:39 -0300

    .

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let chute = prompt('Escolha um número entre 1 e 10');
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    alert('Isso ai! você descobriu o numero secreto (4) ');
+    alert(`Isso ai! você descobriu o numero secreto ${numeroSecreto}`);
 } else {
     alert('Você errou :(')
 }
