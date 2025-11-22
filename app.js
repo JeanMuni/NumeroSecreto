3c596f82b323b0939bfb353139590d3138743208
JeanMuni <144803173+JeanMuni@users.noreply.github.com>
2025-11-22 19:25:22 -0300

    .

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ alert('Boas vindas ao jogo secreto');
 let numeroSecreto = 5;
 let chute = prompt('Escolha u número entre 1 e 30');
 
+// se chute for igual ao número secreto
 if (chute == numeroSecreto ) {
     console.log('Isso ai você descobriu o numero secreto (5) ');
+} else {
+    alert('Você errou :(')
 }
